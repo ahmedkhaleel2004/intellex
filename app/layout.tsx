1bcab7e17eb6f8173c5f52a1a69e4c19307d3e50
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-14 07:01:55 -0500

    leaderboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
+					<div className="fixed h-screen w-full bg-gradient-to-br dark:from-zinc-800 dark:to-zinc-950 from-zinc-50 to-zinc-300 -z-10" />
 					{children}
 				</ThemeProvider>
 			</body>

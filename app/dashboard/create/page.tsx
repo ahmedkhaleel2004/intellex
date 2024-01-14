55c033b86971cf02b848d7965b2fff4f01fb8723
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-14 05:11:43 -0500

    build issues

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
 	Card,
@@ -9,23 +11,39 @@ import {
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@/lib/hooks/use-auth";
+import { useRouter } from "next/navigation";
 
 const Create = () => {
+	const router = useRouter();
+	const userData = useAuth(router, true);
 	// write a function to find matching users
 	const users = [
 		{
 			name: "Ahmed",
 			photoURL:
 				"https://lh3.googleusercontent.com/a/ACg8ocIhT5hvWlrQnHDnPjDTGy_zob50QOMYcOi1MyfWUtQ5PWM=s96-c",
-			description: "I am a software engineer",
+			description: "I am a full stack developer",
+		},
+		{
+			name: "Shaaf",
+			photoURL:
+				"https://lh3.googleusercontent.com/a/ACg8ocIw3mNSUqD9UUA20GWCdyr5QBvj-U3Trcz0GwIjPg8qtPI=s96-c",
+			description: "I am a machine learning and quantum computing researcher",
 		},
 	];
 
 	return (
 		<div className="flex flex-col items-center justify-center max-w-screen-md mx-auto">
 			<h1 className="text-3xl font-bold m-4">Showing matching users</h1>
-			{users.map((user: any) => (
-				<Card className="m-4 min-w-[60%] flex flex-row items-center">
+			<h2 className="text-lg font-muted-foreground">
+				Showing users skilled in {userData?.Q5.join(", ")}.
+			</h2>
+			{users.map((user: any, index) => (
+				<Card
+					key={index}
+					className="m-4 min-w-[60%] flex flex-row items-center"
+				>
 					<CardHeader className="flex-row items-center">
 						<Avatar className="m-4">
 							<AvatarImage src={user.photoURL} alt="User PFP" />
@@ -36,7 +54,7 @@ const Create = () => {
 							<CardDescription>{user.description}</CardDescription>
 						</div>
 					</CardHeader>
-					<Button className="ml-6">Send a message</Button>
+					<Button className="ml-6">Create session</Button>
 				</Card>
 			))}
 		</div>
